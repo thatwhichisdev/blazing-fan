82ceebcba3523fa3ba6be90f49bd2657a5a7ede6
nanobreaker <nan0br3aker@gmail.com>
2026-06-13 18:23:00 +0000

    daemon & tui: add dependencies and draft examples

--- blazing-fan-daemon/src/main.rs
+++ blazing-fan-daemon/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use blazing_fan_proto::{UART_RES_MAX_SIZE, UartResponse};
+use serial2_tokio::SerialPort;
+use tokio::io::AsyncReadExt;
+
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
+    let mut port = SerialPort::open("/dev/ttyACM5", 115200).unwrap();
+
+    loop {
+        let mut rx_buf = [0u8; UART_RES_MAX_SIZE];
+        let Ok(_size) = port.read_exact(&mut rx_buf).await else {
+            continue;
+        };
+
+        let response = postcard::from_bytes::<UartResponse>(&rx_buf[..]).unwrap();
+        match response {
+            UartResponse::Ok => todo!(),
+            UartResponse::Err => todo!(),
+            UartResponse::Status { .. } => todo!(),
+        }
+    }
 }
