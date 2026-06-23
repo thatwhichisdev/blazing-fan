c0d329478c011e0a4f4605fe8e0e489ce978ee46
nanobreaker <nan0br3aker@gmail.com>
2026-06-23 11:40:44 +0000

    temp

--- blazing-fan-daemon/src/adapter/outbound/otel_adapter.rs
+++ blazing-fan-daemon/src/adapter/outbound/otel_adapter.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, time::Duration};
+use std::collections::HashMap;
 
 use blazing_fan_proto::FanTelemetry;
 use opentelemetry::{
--- blazing-fan-daemon/src/core/sysinfo.rs
+++ blazing-fan-daemon/src/core/sysinfo.rs
@@ -1,7 +1,12 @@
-use sysinfo::{Components, CpuRefreshKind, MemoryRefreshKind, RefreshKind, System};
+use sysinfo::{
+    Components, CpuRefreshKind, DiskRefreshKind, Disks, MemoryRefreshKind, Networks,
+    ProcessRefreshKind, RefreshKind, System,
+};
 
 pub struct SystemFetcher {
-    sys: System,
+    system: System,
+    disks: Disks,
+    networks: Networks,
     components: Components,
     refresh: RefreshKind,
 }
@@ -16,22 +21,31 @@ pub struct SystemMetrics {
     pub cpu_usage: f32,
     pub cpu_tmp: f32,
     pub mem_usage: u64,
+    pub swap_usage: u64,
 }
 
 impl SystemFetcher {
     pub fn new() -> Self {
+        let processes_refresh = ProcessRefreshKind::nothing().with_tasks();
         let cpu_refresh = CpuRefreshKind::nothing().with_cpu_usage().with_frequency();
-        let mem_refresh = MemoryRefreshKind::nothing().with_ram();
+        let mem_refresh = MemoryRefreshKind::nothing().with_ram().with_swap();
         let refresh = RefreshKind::nothing()
-            .without_processes()
+            .with_processes(processes_refresh)
             .with_cpu(cpu_refresh)
             .with_memory(mem_refresh);
 
-        let sys = System::new_with_specifics(refresh);
+        let system = System::new_with_specifics(refresh);
         let components = Components::new_with_refreshed_list();
 
+        let disk_refreshes = DiskRefreshKind::nothing().with_storage().with_io_usage();
+        let disks = Disks::new_with_refreshed_list_specifics(disk_refreshes);
+
+        let networks = Networks::new_with_refreshed_list();
+
         Self {
-            sys,
+            system,
+            disks,
+            networks,
             components,
             refresh,
         }
@@ -44,11 +58,11 @@ impl SystemFetcher {
     }
 
     pub fn fetch_metrics(&mut self) -> SystemMetrics {
-        self.sys.refresh_specifics(self.refresh);
+        self.system.refresh_specifics(self.refresh);
+        self.disks.refresh(false);
         self.components.refresh(false);
 
-        let cpu_usage = self.sys.global_cpu_usage();
-        let mem_usage = self.sys.used_memory();
+        let cpu_usage = self.system.global_cpu_usage();
         let cpu_tmp = self
             .components
             .iter_mut()
@@ -57,10 +71,42 @@ impl SystemFetcher {
             .temperature()
             .unwrap_or(0.0);
 
+        let mem_usage = self.system.used_memory();
+        let swap_usage = self.system.used_swap();
+
+        for disk in &self.disks {
+            tracing::info!(
+                "{:?} - disk, {:?} - kind, {:?} - total space, {:?} - available space",
+                disk.name(),
+                disk.kind(),
+                disk.total_space(),
+                disk.available_space()
+            );
+        }
+
+        for (interface, data) in &self.networks {
+            tracing::info!(
+                "{:?} - interface, {:?} - received, {:?} - transmitted",
+                interface,
+                data.received(),
+                data.transmitted()
+            );
+        }
+
+        for component in &self.components {
+            tracing::info!(
+                "{:?} - id, {:?} - label, {:?} - temperature",
+                component.id(),
+                component.label(),
+                component.temperature()
+            );
+        }
+
         SystemMetrics {
             cpu_usage,
             cpu_tmp,
             mem_usage,
+            swap_usage,
         }
     }
 }
